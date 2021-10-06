ce2f55c43f67bab0420420fc3b39624c8383d665
ciphersathi1 <ciphersathi@gmail.com>
2021-10-06 17:51:20 +0530

    func

---
+++ eclipse-workspace/Basics/src/conti.java
@@ -0,0 +1,23 @@
+
+public class conti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//break
+	for (int i=1;i<=10;i++) {
+		if(i==4) {
+			break;
+		}
+		System.out.println(i);
+	}
+	
+	//continue
+	for (int i=1;i<=10;i++) {
+		if(i==4) {
+			continue;
+		}
+		System.out.println(i);
+	}
+}
+}
